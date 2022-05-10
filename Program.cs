52523e890836fcac9f44c918ecc159e401621472
MaiDucAnh <galuathancongz@gmail.com>
2022-05-10 23:43:34 +0700

    commit file phu

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int abc;
+string ta = "Moi nguoi oi";
+var j = 1;
+public class MyClass{
+    
+}
+struct tencuastruct
+{
+    public int x;
+    public int y;
+}
+enum khongbietnokhacvarchonao{
+    Hoi_nguoi_than_khong_biet,
+    ngaitragoogle,
+    phailamsaophailamsao
+}
